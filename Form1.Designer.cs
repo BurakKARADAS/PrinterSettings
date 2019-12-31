abe8d423961be9baf0152239baddd3a99a6f1961
Burak KARADAŞ <burak@atilimyazilim.com>
2019-12-31 14:45:37 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace PrinterSettings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnYenile = new DevExpress.XtraEditors.SimpleButton();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 33);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(737, 467);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnYenile
+            // 
+            this.btnYenile.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnYenile.Image = global::PrinterSettings.Properties.Resources.iconfinder_sync_126579;
+            this.btnYenile.Location = new System.Drawing.Point(0, 0);
+            this.btnYenile.Name = "btnYenile";
+            this.btnYenile.Size = new System.Drawing.Size(737, 33);
+            this.btnYenile.TabIndex = 1;
+            this.btnYenile.Text = "Yenile";
+            this.btnYenile.Click += new System.EventHandler(this.btnYenile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 500);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnYenile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yazıcı Listesi";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private DevExpress.XtraEditors.SimpleButton btnYenile;
+
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Printing;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Management;
+using DevExpress.XtraEditors;
+using Microsoft.Win32;
+namespace PrinterSettings
+{
+    public partial class Form1 : Form
+    {
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = this.Text + " v" + Application.ProductVersion.ToString();
+            var server = new PrintServer();
+            var queues = server.GetPrintQueues(new[] { EnumeratedPrintQueueTypes.Local, EnumeratedPrintQueueTypes.Connections });
+            foreach (var queue in queues)
+            {
+               
+                string printerName = queue.Name;
+                string printerPort = queue.QueuePort.Name;
+              
+
+                if (!printerPort.Contains("192."))
+                {
+                    bool status = Connected(printerName);
+                    GroupControl g = new GroupControl();
+                    g.Size = new Size(this.Width-40, g.Height);
+                    g.Dock = DockStyle.Top;
+
+                    flowLayoutPanel1.Controls.Add(g);
+                    FlowLayoutPanel f = new FlowLayoutPanel();
+                    f.Dock = DockStyle.Fill;
+                    f.FlowDirection = FlowDirection.TopDown;
+                    g.Controls.Add(f);
+                    Label l = new Label();
+                    l.AutoSize = true;
+                    l.Font = new Font("Tahoma", 13);
+                    l.ImageAlign = ContentAlignment.MiddleLeft;
+                    l.Text = "   Yazıcı Adı : " + printerName;
+                    if (status) l.Image = Properties.Resources._1451487868_bullet_green;
+                    else l.Image = Properties.Resources._1451487929_bullet_red;
+                    f.Controls.Add(l);
+
+                    Label l1 = new Label();
+                    l1.AutoSize = true;
+                    l1.Font = new Font("Tahoma", 13);
+                    l1.Text = "   Port : " + printerPort;
+                    f.Controls.Add(l1);
+                }
+                else
+                {
+                    bool status = Ping(printerPort);
+                    GroupControl g = new GroupControl();
+                    g.Size = new Size(this.Width - 40, g.Height);
+                    g.Dock = DockStyle.Top;
+                    flowLayoutPanel1.Controls.Add(g);
+                    FlowLayoutPanel f = new FlowLayoutPanel();
+                    f.Dock = DockStyle.Fill;
+                    f.FlowDirection = FlowDirection.TopDown;
+                    g.Controls.Add(f);
+                    Label l = new Label();
+                    l.AutoSize = true;
+                    l.Font = new Font("Tahoma", 13);
+                    l.ImageAlign = ContentAlignment.MiddleLeft;
+                    l.Text = "   Yazıcı Adı : " + printerName;
+                    if (status) l.Image = Properties.Resources._1451487868_bullet_green;
+                    else l.Image = Properties.Resources._1451487929_bullet_red;
+                    f.Controls.Add(l);
+
+                    Label l1 = new Label();
+                    l1.AutoSize = true;
+                    l1.Font = new Font("Tahoma", 13);
+                    l1.Text = "   Port : " + printerPort;
+                    f.Controls.Add(l1);
+
+                   
+                }
+
+            }
+
+        }
+
+        public bool Connected(string printer_name)
+        {
+            bool isConnected = false;
+
+            try
+            {
+
+
+                ManagementScope scope = new ManagementScope(@"\root\cimv2");
+                scope.Connect();
+
+
+                ManagementObjectSearcher searcher = new
+                 ManagementObjectSearcher("SELECT * FROM Win32_Printer");
+
+                string printerName = "";
+                foreach (ManagementObject printer in searcher.Get())
+                {
+                    printerName = printer["Name"].ToString();
+                    if (printerName.Equals(printer_name))
+                    {
+                        if (printer["WorkOffline"].ToString().ToLower().Equals("true"))
+                        {
+
+                            isConnected = false;
+                        }
+                        else
+                        {
+                            isConnected = true;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+
+            return isConnected;
+        }
+        public bool Ping(string p_IPAddress)
+        {
+            bool isConnected = false;
+            try
+            {
+                IPAddress ip = IPAddress.Parse(p_IPAddress);
+
+                Ping pingSender = new Ping();
+                IPAddress address = IPAddress.Loopback;
+                PingReply reply = pingSender.Send(p_IPAddress);
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    isConnected = true;
+                }
+
+            }
+            catch
+            {
+
+            }
+            return isConnected;
+        }
+
+        private void btnYenile_Click(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Controls.Clear();
+
+            var server = new PrintServer();
+            var queues = server.GetPrintQueues(new[] { EnumeratedPrintQueueTypes.Local, EnumeratedPrintQueueTypes.Connections });
+            foreach (var queue in queues)
+            {
+
+                string printerName = queue.Name;
+                string printerPort = queue.QueuePort.Name;
+
+
+                if (!printerPort.Contains("192."))
+                {
+                    bool status = Connected(printerName);
+                    GroupControl g = new GroupControl();
+                    g.Size = new Size(this.Width - 40, g.Height);
+                    g.Dock = DockStyle.Top;
+
+                    flowLayoutPanel1.Controls.Add(g);
+                    FlowLayoutPanel f = new FlowLayoutPanel();
+                    f.Dock = DockStyle.Fill;
+                    f.FlowDirection = FlowDirection.TopDown;
+                    g.Controls.Add(f);
+                    Label l = new Label();
+                    l.AutoSize = true;
+                    l.Font = new Font("Tahoma", 13);
+                    l.ImageAlign = ContentAlignment.MiddleLeft;
+                    l.Text = "   Yazıcı Adı : " + printerName;
+                    if (status) l.Image = Properties.Resources._1451487868_bullet_green;
+                    else l.Image = Properties.Resources._1451487929_bullet_red;
+                    f.Controls.Add(l);
+
+                    Label l1 = new Label();
+                    l1.AutoSize = true;
+                    l1.Font = new Font("Tahoma", 13);
+                    l1.Text = "   Port : " + printerPort;
+                    f.Controls.Add(l1);
+                }
+                else
+                {
+                    bool status = Ping(printerPort);
+                    GroupControl g = new GroupControl();
+                    g.Size = new Size(this.Width - 40, g.Height);
+                    g.Dock = DockStyle.Top;
+                    flowLayoutPanel1.Controls.Add(g);
+                    FlowLayoutPanel f = new FlowLayoutPanel();
+                    f.Dock = DockStyle.Fill;
+                    f.FlowDirection = FlowDirection.TopDown;
+                    g.Controls.Add(f);
+                    Label l = new Label();
+                    l.AutoSize = true;
+                    l.Font = new Font("Tahoma", 13);
+                    l.ImageAlign = ContentAlignment.MiddleLeft;
+                    l.Text = "   Yazıcı Adı : " + printerName;
+                    if (status) l.Image = Properties.Resources._1451487868_bullet_green;
+                    else l.Image = Properties.Resources._1451487929_bullet_red;
+                    f.Controls.Add(l);
+
+                    Label l1 = new Label();
+                    l1.AutoSize = true;
+                    l1.Font = new Font("Tahoma", 13);
+                    l1.Text = "   Port : " + printerPort;
+                    f.Controls.Add(l1);
+
+
+                }
+            }
+        }
+    }
+}
